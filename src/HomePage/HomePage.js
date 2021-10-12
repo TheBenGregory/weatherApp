c41ec82ce7db2f99f0e75989ee2501e878632845
Ben Gregory <JBGregoryPro@protonmail.com>
2021-10-12 06:49:59 -0500

    attempting exclusion

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
-// import { getWeather } from "../apiManager/apiManager";
+import { api } from "../apiManager/apiManager";
 
 
 
 export const HomePage = () => {
-    const api = {
-        key: "e1d40a2013e49267140f997eab3df080",
-        base: "http://pro.openweathermap.org/data/2.5/"
-      }
+    
     const [weather, setWeather] = useState([])
     const [query, setQuery] = useState([])
 
---
+++ src/apiManager/apiManager.js
@@ -0,0 +1,8 @@
+
+export const api = {
+    key: "e1d40a2013e49267140f997eab3df080",
+    base: "http://pro.openweathermap.org/data/2.5/"
+  }
+
+
+
